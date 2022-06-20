b87e8cb791c4582f806afe7a4937e953540ad2c7
denisdiaconu <denisdiaconu117@yahoo.com>
2022-06-20 04:18:34 +0200

    Add react-unicons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 function App() {
   return (
     <div className="App">
-      <h2>hello</h2>
+      <h2 className="text-2xl font-bold text-black">hello</h2>
     </div>
   );
 }
