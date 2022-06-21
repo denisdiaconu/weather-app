16afc7d4e69cdbdf9f8e8ebbe67d53d5af1e51cd
denisdiaconu <denisdiaconu117@yahoo.com>
2022-06-21 03:01:25 +0200

    Add api

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import Inputs from './components/Inputs';
 import TimeCity from './components/TimeCity';
 import TempAndDetails from './components/TempAndDetails';
 import Forecast from './components/Forecast';
+import getFormatData from './api/weatherApi';
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormatData({q: "london"});
+    console.log(data)
+  };
+
+  fetchWeather();
+
   return (
     <div className="max-w-screen-md mx-auto h-fit mt-5 py-6 px-32 shadow-lg shadow-gray-500 bg-gradient-to-br from-orange-400 to-orange-700">
       <Buttons />
--- src/api/weatherApi.js
+++ src/api/weatherApi.js
@@ -1,2 +1,27 @@
-const API_KEY = process.env.REACT_APP_DEFAULT;
+import { data } from "autoprefixer";
 
+const API_KEY = process.env.REACT_APP_DEFAULT;
+const BASE_LINK = 'https://api.openweathermap.org/data/2.5';
+
+const getData = (type, searchParams) => {
+    const url = new URL(BASE_LINK + '/' + type);
+    url.search = new URLSearchParams({...searchParams, appid:API_KEY})
+    return fetch(url).then((resolution) => resolution.json());
+};
+
+const formatForecastWeather = (data) => {
+    let
+}
+
+const getFormatData = async (searchParams) => {
+    const formatWeatherNow = await getData('weather', searchParams)
+    const { coord } = formatWeatherNow;
+    const { lat, lon } = coord;
+    const formatForecast = await getData('onecall', {lat, lon, exclude: 'current,minutely,alerts', units: 'metric'}).then(
+        formatForecastWeather
+    )
+
+    return formatWeatherNow
+}
+
+export default getFormatData;
