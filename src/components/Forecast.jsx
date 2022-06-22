d0c20c6661b996401c28856984d95d8b1fef65bb
denisdiaconu <denisdiaconu117@yahoo.com>
2022-06-22 12:17:33 +0200

    Add changes to forecast

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -9,7 +9,7 @@ function Forecast({name, list}) {
             <p className='text-white font-semibold ml-4 md:ml-6'>{name}</p>
         </div>
         <hr className='mb-4 w-11/12 mx-auto' />
-        <div className='flex flex-row flex-wrap justify-between items-center text-white'>
+        <div className='md:flex md:flex-row md:flex-wrap justify-between items-center grid gap-7 md:gap-0 mx-10 md:mx-0 grid-cols-3 text-white'>
             {list.map(item => (
                 <div key={uuidv4()} className='flex flex-col my-2 py-2 justify-center items-center px-3 bg-gradient-to-t rounded'>
                     <p className='font-normal text-sm'>{item.title}</p>
